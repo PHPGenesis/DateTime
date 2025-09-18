9528fbd8e3041a36beede315b5e35ddd7c7d5ff5
GenesisBot <ghbot@phpgenesis.com>
2025-09-18 18:12:56 +0000

    [DateTime] Add IDateTime Interface (#158)

---
+++ src/IDateTime.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copyright (c) 2025. Encore Digital Group.
+ * All Right Reserved.
+ */
+
+namespace PHPGenesis\DateTime;
+
+use Carbon\CarbonInterface;
+
+interface IDateTime extends CarbonInterface {}
